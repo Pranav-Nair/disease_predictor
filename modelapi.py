2df112ce92b924d35d2b465a3075c1e99c6fc8a5
Pranav-Nair <66962175+Pranav-Nair@users.noreply.github.com>
2023-03-16 12:44:56 +0530

    API for lung cancer prediction

--- modelapi.py
+++ modelapi.py
@@ -132,4 +132,164 @@ def predict_heartissue():
     else:
         return jsonify({"result":result}),200
 
+
+@mlcore.get("/lungcancer")
+def predictLungCancer():
+    gender = request.json.get("gender",None)
+    age = request.json.get("age",None)
+    smoking = request.json.get("smoking",None)
+    yellow_fingers = request.json.get("yellow_fingers",None)
+    anxity = request.json.get("anxity",None)
+    peer_pressure = request.json.get("peer_pressure",None)
+    chronicDisease = request.json.get("chronic_disease",None)
+    fatigue = request.json.get("fatigue",None)
+    allergy = request.json.get("allergy",None)
+    wheezing = request.json.get("wheezing",None)
+    alchol = request.json.get("alchol",None)
+    cough = request.json.get("cough",None)
+    shortness_of_breath = request.json.get("shortness_of_breath",None)
+    swallowing_difficulty = request.json.get("swallowing_difficulty",None)
+    chest_pain = request.json.get("chest_pain",None)
+    
+    if not gender or not age or smoking is None or yellow_fingers is None or anxity is None or \
+        peer_pressure is None or chronicDisease is None or fatigue is None or allergy is None or \
+        wheezing is None or alchol is None or cough is None or shortness_of_breath is None or \
+        swallowing_difficulty is None or chest_pain is None:
+
+        return jsonify({"error":"missing fields","required fields":["gender","age","smoking","yellow_fingers",\
+                                                                    "anxity","peer_pressure","chronic_disease","fatigue","allergy","wheezing","alchol","cough",\
+                                                                        "shortness_of_breath","swallowing_difficulty","chest_pain"]}),400
+    
+    if gender not in ["male","female"]:
+        return jsonify({"error":"gender can only be male or female"}),400
+    
+    if age <=0:
+        return jsonify({"error":"age cannot be less than or equal to 0"}),400
+    
+    if not isinstance(smoking,bool):
+        return jsonify({"error":"smoking can be true or false"})
+    
+    if not isinstance(yellow_fingers,bool):
+        return jsonify({"error":"yellow_fingers can be true or false"})
+    
+    if not isinstance(anxity,bool):
+        return jsonify({"error":"anxity can be true or false"})
+    
+    if not isinstance(peer_pressure,bool):
+        return jsonify({"error":"peer_pressure can be true or false"})
+    
+    if not isinstance(chronicDisease,bool):
+        return jsonify({"error":"chronic_disease can be true or false"})
+    
+    if not isinstance(fatigue,bool):
+        return jsonify({"error":"fatigue can be true or false"})
+    
+    if not isinstance(allergy,bool):
+        return jsonify({"error":"allergy can be true or false"})
+    
+    if not isinstance(wheezing,bool):
+        return jsonify({"error":"wheezing can be true or false"})
+    
+    if not isinstance(alchol,bool):
+        return jsonify({"error":"alchol can be true or false"})
+    
+    if not isinstance(cough,bool):
+        return jsonify({"error":"cough can be true or false"})
+    
+    if not isinstance(shortness_of_breath,bool):
+        return jsonify({"error":"shortness_of_breath can be true or false"})
+    
+    if not isinstance(swallowing_difficulty,bool):
+        return jsonify({"error":"swallowing_difficulty can be true or false"})
+    
+    if not isinstance(chest_pain,bool):
+        return jsonify({"error":"chest_pain can be true or false"})
+    
+    if gender == 'male':
+        gender = 1
+    else:
+        gender = 0
+    
+    if smoking:
+        smoking = 2
+    else:
+        smoking = 1
+
+    if yellow_fingers:
+        yellow_fingers = 2
+    else:
+        yellow_fingers = 1
+    
+    if anxity:
+        anxity = 2
+    else:
+        anxity = 1
+    
+    if peer_pressure:
+        peer_pressure = 2
+    else:
+        peer_pressure = 1
+
+    if chronicDisease:
+        chronicDisease = 2
+    else:
+        chronicDisease = 1
+
+    if fatigue:
+        fatigue = 2
+    else:
+        fatigue = 1
+
+    if allergy:
+        allergy = 2
+    else:
+        allergy = 1
+
+    if wheezing:
+        wheezing = 2
+    else:
+        wheezing = 1
+
+    if alchol:
+        alchol = 2
+    else:
+        alchol = 1
+
+    if cough:
+        cough = 2
+    else:
+        cough = 1
+
+    if shortness_of_breath:
+        shortness_of_breath = 2
+    else:
+        shortness_of_breath = 1
+
+    if swallowing_difficulty:
+        swallowing_difficulty = 2
+    else:
+        swallowing_difficulty = 1
+
+    if chest_pain:
+        chest_pain = 1
+    else:
+        chest_pain = 0
+    
+    data = [[gender,age,smoking,yellow_fingers,anxity,peer_pressure,chronicDisease,fatigue,allergy,wheezing,alchol,cough,shortness_of_breath,swallowing_difficulty,chest_pain]]
+    model = joblib.load("models/lungcancer.joblib")
+    res = model.predict(data)
+    result = "no"
+    prevstrats = []
+    if res == "YES":
+        result = "yes"
+        f = open("preventions/lungcancer.txt","r")
+        prev = f.readlines()
+        f.close()
+        for line in prev:
+            prevstrats.append(line.strip('\n'))
+        return jsonify({"result":result,"prevention methods":prevstrats}),200
+    elif res == "NO":
+        return jsonify({"result":result}),200
+
+
     
